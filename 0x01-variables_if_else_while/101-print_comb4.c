24de7aa8e71ca0cd4ab0a641e75ef76a4c54969e
iamjudeicon <judecchukwuemeka@gmail.com>
2022-10-15 20:47:46 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+  * main - (entry point) prints all possible combinations
+  * of three digits
+  * Return: Success (0)
+  */
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 48; x < 58; x++)
+	{
+		for (y = 49; y < 58; y++)
+		{
+			for (z = 50; z < 50; z++)
+			{
+				if (z > y && y > x)
+				{
+
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x != 55 || y != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
