c2b03d7b94598741c70365968217595609e67ccf
iamjudeicon <judecchukwuemeka@gmail.com>
2022-10-20 02:18:39 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * _isalpha - prints function that checks for 
+  * _isalpha - prints function that checks for
   * alphabetic character
   * @c: the character to be checked
   * Return: 1 if c is a letter, 0 otherwise
