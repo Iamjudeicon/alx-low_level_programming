6b1b30df7f054e83f069a43d21de4dca1ef05ce9
iamjudeicon <judecchukwuemeka@gmail.com>
2022-10-21 10:27:28 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints the numbers from 0 - 9
+  */
+void print_numbers(void)
+{
+	int 1;
+
+	for (i = 0, i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
